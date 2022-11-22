4f70d895f71e4ea8469a35d5ca1dea0c624344f5
urfavteddybear <97431620+urfavteddybear@users.noreply.github.com>
2022-11-22 23:21:22 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,16 +3,7 @@
  * @param {import("../lib/DiscordMusicBot")} client
  */
 module.exports = (client) => {
-	if(client.manager)
-	client.manager.init(client.user.id);
-
-	const activities = client.config.presence.activities;
-	setInterval(() => {
-		const index = Math.floor(Math.random() * (activities.length - 1));
-		client.user.setActivity({
-			name: activities[index].name, 
-			type: activities[index].type
-		});
-	}, 10000);
-	client.log("Successfully logged in as " + client.user.tag);
+  client.manager.init(client.user.id);
+  client.user.setPresence(client.config.presence);
+  client.log("Successfully Logged in as " + client.user.tag);
 };
